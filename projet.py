45efb8d54fbac93d4bb15f2e247af76a1cb9dff6
Azeddine0 <62209358+Azeddine0@users.noreply.github.com>
2020-03-16 18:05:58 +0100

    Add files via upload

---
+++ projet.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 17:13:52 2020
+
+@author: Azeddine
+"""
+import numpy as np
+from scipy import linalg as LA
+
+
+
+def vect_alea(n):
+    A= 101*np.random.rand(n,n)
+    return A
+
+A=vect_alea(5)
+print(A)
+
+
+def lin_inde(mat) :
+    det=LA.det(mat)
+    return det
+
+def normaliser(v):
+    
+
+
+def orthonormaliser(mat):
+    
+
+print(A)
+
+if(lin_inde(A)) : print("La famille est une base")
+else : print("la famille n'est pas une base")
+
+Q,R=LA.qr(A)
+print(Q)
+print(R)
+
+
+
+
+B =  np.array([[2,-2,1], [-2,2,0],[1,1,2]])
+print(B)
+print(lin_inde(B))
+print(orthonormaliser(B))

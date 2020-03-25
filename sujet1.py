1911f61643777719d7a7a65ed4bdccbd8b74e20d
eguientat <62184912+eguientat@users.noreply.github.com>
2020-03-25 13:46:44 +0100

    Update sujet1.py

--- sujet1.py
+++ sujet1.py
@@ -7,64 +7,62 @@ Created on Mon Mar 16 18:30:25 2020
 
 
 #sujet 1
-import random
+from random import *
+import numpy.polynomial.polynomial as nppol
 
 def premier(n):
     x=0
     for i in range(2, n):
         if(n%i==0):
             x=x+1
-
-    if(x>=1):
+        
+    if(x>=1): 
         return 0
-    else :
+    else : 
         return 1
-
+            
 print(premier(97))
 
 
-def pluspetitque(n,x,j):
+def pluspetitque(n):
+    liste = []
     print("Les nombres premiers inférieurs à ",n," sont ")
     for i in range (1,n):
         if(premier(i)==1):
-            if(i>x and j<3):
-                x=i
-                j=j+1
-    print(x)
-    print("\n")
+                liste.append(i)
+                print(i)            
+                print("\n")         
+    return liste           
+    
+    
+print(pluspetitque(150,0,0))
+
+def restechinois(liste,n):
+    M=0
+    x=0
+    for i in liste:
+        M=M*i
+    for i in range(1,n):
+        m=liste[i]
+        N=M/m
+        y=algoEuclide(M,m)
+        a=randint(1,m-1)
+        x=x+(a*N*y)
+    x=x%M
     return x
 
 
-print(pluspetitque(150,0,0))
-m1=pluspetitque(100, 0, 0)
-m2=pluspetitque(100,m1,1)
-m3=pluspetitque(100, m2, 2)
-
-m1=7
-m2=11
-m3=13
-
-
-def restechinois(m1,m2,m3):
-    M=m1*m2*m3
-    M1=M/m1
-    M2=M/m2
-    M3=M/m3
+def estIrreductible(p):
+    if(p.degree()==1): 
+        return 1
+    else :
+        if(p.degree()==2 and (p[1]*p[1]-4*p[2]*p[0])<0):
+            return 1
+        else:
+            return 0
     
-    y1=(1/M1)%m1#implémenter l'algorithme d'Euclide étendu car c'est faux sinon pour les y
-    y2=(1/M2)%m2
-    y3=(1/M3)%m3
-    print(y1)
-    a1=random.randint(1,m1-1)
-    print("a1 ",a1)
-    print("\n")
-    a2=random.randint(1,m2-1)
-    print("a2",a2)
-    print("\n")
-    a3=random.randint(1,m3-1)
-    print("a3 ",a3)
-    print("\n")
-    res = ((a1*M1*y1)+(a2*M2*y2)+(a3*M3*y3))%M
-    return res
+    
 
-print(restechinois(m1,m2,m3))
+
+
+    

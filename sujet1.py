01c656e19b828bcb58d59a22695c762a256400a3
eguientat <62184912+eguientat@users.noreply.github.com>
2020-03-29 12:46:12 +0200

    sujet1

--- sujet1.py
+++ sujet1.py
@@ -15,12 +15,12 @@ def premier(n):
     for i in range(2, n):
         if(n%i==0):
             x=x+1
-        
-    if(x>=1): 
+
+    if(x>=1):
         return 0
-    else : 
+    else :
         return 1
-            
+
 print(premier(97))
 
 
@@ -30,12 +30,35 @@ def pluspetitque(n):
     for i in range (1,n):
         if(premier(i)==1):
                 liste.append(i)
-                print(i)            
-                print("\n")         
-    return liste           
-    
-    
-print(pluspetitque(150,0,0))
+                print(i)
+                print("\n")
+    return liste
+
+
+print(pluspetitque(150)
+
+def algoEuclide(b,n):
+    n2=n
+    u=0
+    v=1
+    q=n2//b
+    r=n2-q*b
+    while(r>0):
+        temp=u-q*v
+        if(temp>=0):
+            temp=temp%n
+        else:
+            temp=n-((-temp)%n)
+        u=v
+        v=temp
+        n2=b
+        b=r
+        q=n2//b
+        r=n2-q*b
+    if(b!=1):
+        return 0#b n'a pas d'inverse de modulo n
+    else :
+        return v
 
 def restechinois(liste,n):
     M=0
@@ -53,16 +76,10 @@ def restechinois(liste,n):
 
 
 def estIrreductible(p):
-    if(p.degree()==1): 
+    if(p.degree()==1):
         return 1
     else :
         if(p.degree()==2 and (p[1]*p[1]-4*p[2]*p[0])<0):
             return 1
         else:
             return 0
-    
-    
-
-
-
-    
